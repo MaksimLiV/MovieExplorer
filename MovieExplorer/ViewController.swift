e183beff078521beb13628f6134b879d1810bd5b
Maksim Li <maximlee.ml@gmail.com>
2025-08-19 22:05:57 +0300

    Initial Commit

---
+++ MovieExplorer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieExplorer
+//
+//  Created by Maksim Li on 19/08/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
